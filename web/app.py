baa41230429190d10b3e6a02e5ef4b856e9a5e06
zj75968-cpu <zj57938@gmail.com>
2026-05-15 21:56:46 +0800

    chore: remove reload trigger comment

--- web/app.py
+++ web/app.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2025 AIDC-AI  # reload trigger
+# Copyright (C) 2025 AIDC-AI
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
